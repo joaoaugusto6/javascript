a95aa56c6bf5e7504f3af5287052db1135c5540e
Joaoauguto6 <joaosouzaaugusto6@outlook.com>
2026-03-12 19:33:04 -0300

    estudando for, do e white

---
+++ exercicios/ex008/do,white.js
@@ -0,0 +1,15 @@
+var c = Number(1) // a variavel c recebe 1
+
+ do { // primeira maneira de fazer
+    console.log(`Passo ${c}.`) //mostrar o valor da variavel c
+    c++ // significa c+1
+} while (c <= 10) // significa que c é menor ou igual a 10
+
+/* // segunda maneira
+while (c <= 10) {
+    console.log(`Passo ${c}.`)
+    c++
+}
+*/ 
+
+console.log('Terminou')
---
+++ exercicios/ex008/for.js
@@ -0,0 +1,13 @@
+/*
+var c = 1 
+while (c <= 10) {
+    console.log(c)
+    c++
+}
+*/
+
+for (c = 1; c <= 10; c++) { // se a variavel c é igual a 1; c é maior ou igual a 10; c é igual a 1 + 1
+    console.log(c)
+}
+
+console.log('terminou')
