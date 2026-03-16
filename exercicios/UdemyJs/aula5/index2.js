63ff363ba3b0d4d14303a9408d0949703dda89b7
Joaoauguto6 <joaosouzaaugusto6@outlook.com>
2026-03-16 14:47:12 -0300

    correção no código aula5

--- exercicios/UdemyJs/aula5/index2.js
+++ exercicios/UdemyJs/aula5/index2.js
@@ -1,8 +1,21 @@
+/*
 let nome; // declarou a variavel
 nome = 'João'; // inicializou a variavel
-console.log(nome)
-nome = 'Augusto';
-console.log(nome);
+console.log(nome); // a variavel apareçe
+nome = 'Augusto'; // tocou o valor da variavel
+console.log(nome); // apareçe a variavel com o valor trocado
 
-let nomi;
-console.log('teste')
+console.log('teste');
+*/
+
+// Não podemos criar variaveis com palavras reservadas
+// Váriaveis precisamter nomes significativos
+// Não pode começar o nome da variável com um numero
+// Não podem conter espaços ou traços
+// Utilizamos camelCase
+// Não podemos declarar variáveis com let
+// NÃO UTILIZAR VAR, UTILIZAR LET.
+let nomeCliente = 'joao'
+let nomecliente = 'augusto'
+
+console.log(nomeCliente, nomecliente) // À diferença entre minusculas e maiusculas
