688fef68b986ccbeedadad5915833aa94cc8366e
Joaoauguto6 <joaosouzaaugusto6@outlook.com>
2026-03-16 20:23:25 -0300

    criação da aula6

---
+++ exercicios/UdemyJs/aula6/index.js
@@ -0,0 +1,10 @@
+// Não podemos criar constantes com palavras reservadas
+// Constantes precisamter nomes significativos
+// Não pode começar o nome da constante com um numero
+// Não podem conter espaços ou traços
+// Utilizamos camelCase
+// Não pode modificar o valor de uma constante
+// NÃO UTILIZAR VAR, UTILIZAR CONST.
+
+const nome = 'João' // Não dodemos trocar o valor da const e temos que declarar obrigatóriamente na hora.
+console.log(nome)
