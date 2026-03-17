79ca9afd5885fd628bec7dbc223d05c47a839879
Joaoauguto6 <joaosouzaaugusto6@outlook.com>
2026-03-17 15:56:41 -0300

    correção no código aula7

--- exercicios/UdemyJs/aula7/index.js
+++ exercicios/UdemyJs/aula7/index.js
@@ -1,6 +1,6 @@
 /*
 João Augusto de Souza tem 13 anos, pesa 46 kg
-tem 1.6 de altura e seu IMC é de 25.925925925924
+tem 1.6 de altura e seu IMC é de 17.97
 João Augusto nasceu em 2012
 */
 
@@ -9,8 +9,12 @@ const sobrenome = 'de Souza';
 const idade = 13;
 const peso = 46;
 const alturaEmM = 1.60;
-let  imc; // peso / (altura * altura)
-let anoNascimento;
 
+let imc;
 imc = peso / (alturaEmM * alturaEmM);
-console.log(imc);
+
+let ano = 2012;
+
+console.log(`${nome} ${sobrenome} tem ${idade} anos, pesa ${peso} kg`);
+console.log(`tem ${alturaEmM} de altura e o seu IMC é de ${imc}`);
+console.log(`${nome} nasceu em ${ano}`);
