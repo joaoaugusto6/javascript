83932a1525c254b7cb281044ed9e73023e104e2e
Joaoauguto6 <joaosouzaaugusto6@outlook.com>
2026-03-17 17:06:23 -0300

    criação da aula8

---
+++ exercicios/UdemyJs/aula8/index.js
@@ -0,0 +1,10 @@
+//String, Number, undefined, null, boolean, symbol
+
+const nome = 'Luiz' // String
+const nome1 = "Luiz" // String
+const nome2 = `Luiz` // String
+const num1 = 10 // Number
+const num2 = 10.52 // Number
+let nomeAluno; // undefined = não aponta pra local nenhum na memória
+const sobrenome = null // Nulo = não aponta pra local nenhum na memória
+const boolean = true // Boolean = true, false (Lógico)
