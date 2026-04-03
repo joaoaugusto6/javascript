8c1fcee79d8dc7cef8deec889b011bdead688f41
Joaoauguto6 <joaosouzaaugusto6@gmail.com>
2026-04-03 12:18:17 -0300

    correção no código da aula21

--- exercicios/UdemyJs/aula21/operadorescomp01.js
+++ exercicios/UdemyJs/aula21/operadorescomp01.js
@@ -13,13 +13,13 @@ Opreradores de comparação
 const num1 = 10;
 const num2 = 20;
 let comp1 = num1 > num2; // > maior que
-console.log(comp);
+console.log(comp1);
 
 const comp2 = num1 >= num2; // >= maior ou igual a
-console.log(comp);
+console.log(comp2);
 
 const comp3 = num1 < num2; // < menor que
-console.log(comp);
+console.log(comp3);
 
 const comp4 = num1 <= num2; // <= menor ou igual a
-console.log(comp);
+console.log(comp4);
