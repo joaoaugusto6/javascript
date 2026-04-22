a72554881c05f88c839d6a8cb2f9dfc376bf5dbb
Joaoauguto6 <joaosouzaaugusto6@gmail.com>
2026-04-22 16:15:47 -0300

    criação da aula34

---
+++ exercicios/UdemyJs/aula34/index01.js
@@ -0,0 +1,29 @@
+// console.log('Linha 0');
+// console.log('Linha 1');
+// console.log('Linha 2');
+// console.log('Linha 3');
+// console.log('Linha 4');
+// console.log('Linha 5');
+
+// i = index
+for (let i = 0; i <= 20; i++) {
+    console.log(`Linha ${i}`);
+}
+
+console.log('-------------------');
+
+for (let i = 900; i <= 1000; i += 20) {
+    console.log(`Linha ${i}`);
+}
+
+console.log('-------------------');
+
+for (let i = 100; i >= 0; i -= 20) {
+    console.log(`Linha ${i}`);
+}
+
+console.log('-------------------');
+
+for (let i = -100; i <= 0; i += 10) {
+    console.log(`Linha ${i}`);
+}
---
+++ exercicios/UdemyJs/aula34/index02.js
@@ -0,0 +1,6 @@
+//               0         1       2      3       4
+const frutas = ['Banana', 'Maçã', 'Pêra', 'Uva', 'Melancia'];
+
+for (let i = 0; i < frutas.length; i++) {
+    console.log(`Índice ${i}`,frutas[i]);
+}
