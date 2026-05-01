794caa78827aa925ab6a85a590581a91ba322438
Joaoauguto6 <joaosouzaaugusto6@gmail.com>
2026-05-01 17:10:12 -0300

    criação da aula41

---
+++ exercicios/UdemyJs/aula41/index01.js
@@ -0,0 +1,4 @@
+// Escreva uma função que recebe dois números e retorne o maior deles.
+const max = (a, b) => a > b ? a : b;
+console.log(max(5, 10));
+// este é o jeito mais simples de resolver esse problema.
